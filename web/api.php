fc027324efefbd49324d86dfb1740046044bf1e8
Rahul <87630115+XTelegram@users.noreply.github.com>
2022-08-31 00:36:42 +0530

    Update api.php

--- web/api.php
+++ web/api.php
@@ -190,7 +190,7 @@ curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
 curl_setopt($ch, CURLOPT_COOKIEFILE, getcwd().'/cookie.txt');
 curl_setopt($ch, CURLOPT_COOKIEJAR, getcwd().'/cookie.txt');
-curl_setopt($ch, CURLOPT_POSTFIELDS, 'key=pk_live_4VRhmzVJRIlREvyaOF05DbB9&_stripe_account=acct_102opF2fOy8ULYr0&payment_user_agent=stripe.js%2Fa44017d&card[type]=mastercard&card[number]='.$cc.'&card[cvc]='.$cvv.'&card[expiry]='.$mes.'%2F'.$ano.'&card[name]='.$name.'+'.$last.'&card[address_line1]='.$street.'&card[address_city]='.$city.'&card[address_state]='.$state.'&card[address_zip]='.$postcode.'&card[address_country]=US&card[exp_month]='.$mes.'&card[exp_year]='.$ano.'');
+curl_setopt($ch, CURLOPT_POSTFIELDS, 'card[name]=Jack+cooper&card[number]=4115680132330807&card[cvc]=884&card[exp_month]=08&card[exp_year]=23&card[address_zip]=10080&guid=1161117f-a174-4a87-a73d-d780b28930aa11a021&muid=b5195db8-c680-4711-92ab-1da891313132033050&sid=76a4ae12-b4e1-400e-9918-799386a0088b701be4&payment_user_agent=stripe.js%2F7df5c2138%3B+stripe-js-v3%2F7df5c2138&time_on_page=140036&key=pk_live_4VRhmzVJRIlREvyaOF05DbB9&_stripe_account=acct_102opF2fOy8ULYr0&pasted_fields=number');
 $result1 = curl_exec($ch);
 $token = trim(strip_tags(getStr($result1,'"id": "','"')));
 
